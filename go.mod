065ccceb40b4ef73d0c0fd24229bded3286f4ad6
Christopher Hayles <christopher@hayles.email>
2021-07-21 13:02:39 -0700

    Added crypto library reference.

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module github.com/chrishayles/xrplgo
+
+go 1.16
+
+require golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
--- xrpl/core/cryptography/ed25519.go
+++ xrpl/core/cryptography/ed25519.go
@@ -1,1 +1,13 @@
 package cryptography
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/ed25519"
+)
+
+//Test - Placeholder function for library reference.
+func Test() string {
+	pub, pri, _ := ed25519.GenerateKey(nil)
+	return fmt.Sprintf("Public: %s\n\nPrivate: %s\n", pub, pri)
+}
